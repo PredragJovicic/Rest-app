8333e52f1b298a6e42b4a499f78e2b581037ff1c
PredragJovicic <jovicicpr@gmail.com>
2018-03-26 12:08:05 +0200

    doc

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-    \App\Http\Middleware\cors::class,
+		\App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \App\Http\Middleware\cors::class,
+            \App\Http\Middleware\Cors::class,
         ],
     ];
 
@@ -61,6 +61,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 		'admin' => \App\Http\Middleware\CheckAdmin::class,
 		'RegisterContact' => \App\Http\Middleware\RegisterContact::class, 
-		'cors' => \App\Http\Middleware\cors::class,
+		'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
